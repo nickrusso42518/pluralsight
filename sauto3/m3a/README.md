c10e112976ec93d82c5be5844587e191a75dded5
Nicholas Russo <njrusmc@gmail.com>
2023-10-24 13:57:46 -0400

    xdr draft

---
+++ sauto3/m3a/README.md
@@ -0,0 +1,5 @@
+# Module 3a - Unifying Threat Response with Cisco XDR (New)
+This directory contains code for the Cisco Extended Detection and
+Response (XDR) service, which is the next-iteration of SecureX. It
+is a new addition to the course and includes a small Python SDK
+and example script to inspect some text observables (inputs).
--- sauto3/m3a/check_threats.py
+++ sauto3/m3a/check_threats.py
@@ -10,7 +10,48 @@ import os
 from cisco_xdr import CiscoXDR
 
 
+def _process_refer(data):
+    """
+    Process the refer data, focusing on actions and URLs to check.
+    """
+    for item in data["data"]:
+        print(f"Action: {item['description']}/URL: {item['url']}")
+
+
+def _process_deliberate(data):
+    """
+    Process the deliberate data, focusing on verdicts/dispositions.
+    """
+    for item in data["data"]:
+        if not "verdicts" in item["data"].keys():
+            continue
+
+        for doc in item["data"]["verdicts"]["docs"]:
+            obs = doc["observable"]["value"]
+            print(
+                f"{obs}: {doc['disposition_name']}, verdict"
+                f" valid until {doc['valid_time']['end_time']}"
+            )
+
+
+def _process_observe(data):
+    """
+    Process the observe (detailed) data, focusing on the judgement
+    severity and confidence levels.
+    """
+    for item in data["data"]:
+        if "judgements" in item["data"].keys():
+            for doc in item["data"]["judgements"]["docs"]:
+                obs = doc["observable"]["value"]
+                print(f"{obs}: Sev {doc['severity']}/Conf {doc['confidence']}")
+                print(f"  Judgement reason: {doc['reason']}")
+
+
 def main():
+    """
+    Execution starts here.
+    """
+
     # Create a new XDR instance with default (DevNet LL) parameters
     xdr = CiscoXDR.make_devnet_client()
 
@@ -32,16 +73,20 @@ def main():
         )
 
         # Enrich the observables using three different techniques
-        enrich_actions = ["observe", "refer", "deliberate"]
+        enrich_actions = ["refer", "deliberate", "observe"]
 
         # Iteratively apply the aforementioned techniques
         for enrich_action in enrich_actions:
-            print(f"{enrich_action}: {len(observables)} observables")
+            print(f"\n{60 * '-'}\n{enrich_action.upper()}")
 
             # API is easy to use; many requests use the exact output
             # from the inspection process (list of observables)
             url = f"iroh-enrich/{enrich_action}/observables"
             enrich_data = xdr.req(url, method="post", jsonbody=observables)
+
+            # Call the proper method, based on action name, to process result
+            globals()[f"_process_{enrich_action}"](enrich_data)
+
 
 if __name__ == "__main__":
     main()
